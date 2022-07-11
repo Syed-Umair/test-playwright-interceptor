261845462175a8a9686dd65ddeeb5f62e0fcca18
Syed Umair <syed.yousuff@anywhere.co>
2022-07-12 01:08:20 +0530

    updated config

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,8 +6,8 @@ test('should login and load the test calendar sdk', async ({ page }) => {
         url: 'https://storage.googleapis.com/anywhere-components/dev/live/sch-800/anywhere-calendar.js'
     }));
     await page.goto('https://go.setmore.com');
-    await page.locator('#email.email-field').fill(process.env.login as string);
-    await page.locator('#password.password-field').fill(process.env.password as string);
+    await page.locator('#email.email-field').fill(process.env.LOGIN as string);
+    await page.locator('#password.password-field').fill(process.env.PASSWORD as string);
     await page.locator('#login-now').click();
     await page.waitForNavigation({
         url: 'https://go.setmore.com/calendar'
