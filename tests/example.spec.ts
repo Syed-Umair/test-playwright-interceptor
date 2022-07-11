c50ef353cc72572eff69e4c407cb3a74184334d3
Syed Umair <syed.yousuff@anywhere.co>
2022-07-12 00:49:11 +0530

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('should login and load the test calendar sdk', async ({ page }) => {
+    await page.route(/(anywhere-calendar\.js$)/, route => route.continue({
+        url: 'https://storage.googleapis.com/anywhere-components/dev/live/sch-800/anywhere-calendar.js'
+    }));
+    await page.goto('https://go.setmore.com');
+    await page.locator('#email.email-field').fill('<login here>');
+    await page.locator('#password.password-field').fill('<password here>');
+    await page.locator('#login-now').click();
+    await page.waitForNavigation({
+        url: 'https://go.setmore.com/calendar'
+    });
+    await page.waitForSelector('anywhere-calendar');
+    const calendar = await page.$('anywhere-calendar');
+    await expect(await calendar?.evaluate(node => node.shadowRoot?.textContent)).toBe('Hello Syed!');
+});
